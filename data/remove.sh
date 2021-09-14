e8d914abb88972a26b05247e82b1dddba843c15b
coryo12345 <corydio12@gmail.com>
2021-09-14 19:40:13 -0400

    remove script will remove network

--- data/remove.sh
+++ data/remove.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # this is only necessary if you didn't gracefully exit from start.sh
+source ./env
+
 CONTAINER=wle_db
 
-docker stop $CONTAINER && docker rm $CONTAINER
+docker stop $CONTAINER
+docker rm $CONTAINER
+
+docker network rm $WLE_NETWORK
