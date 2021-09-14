5417a63f2935bb7a7e005d1c4e1feea8e40baf46
coryo12345 <corydio12@gmail.com>
2021-09-14 17:13:08 -0400

    adding network support to data script

--- data/start.sh
+++ data/start.sh
@@ -7,8 +7,16 @@ source ./env
 # container name
 CONTAINER=wle_db
 
+if [[ $(docker network ls | grep ' '$WLE_NETWORK' ') ]]; then
+    echo "network already exists, continuing..."
+else
+    echo "network doesn't exist. creating..."
+    docker network create $WLE_NETWORK
+fi
+
 docker run \
     --name $CONTAINER \
+    --network $WLE_NETWORK \
     -e POSTGRES_PASSWORD=$PG_PASSWORD \
     -e POSTGRES_USER=$PG_USERNAME \
     -e POSTGRES_DB=$PG_DB \
